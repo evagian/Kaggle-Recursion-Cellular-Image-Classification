4c56b117e6df52e89692abe6941d6573c47bd725
Eva Giannatou <eva_giannatou@yahoo.com>
2019-09-05 10:24:29 +0300

    Update 2a_unzip_images_train.py

--- 2a_unzip_images_train.py
+++ 2a_unzip_images_train.py
@@ -1,3 +1,5 @@
+# reads the names of the train balanced subsample and unzips train images into the '../input/train/' folder
+
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
