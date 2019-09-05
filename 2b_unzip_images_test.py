788bdeafe594f69ad5bbf6890f8e02383bd78663
Eva Giannatou <eva_giannatou@yahoo.com>
2019-09-05 10:25:06 +0300

    Update 2b_unzip_images_test.py

--- 2b_unzip_images_test.py
+++ 2b_unzip_images_test.py
@@ -1,3 +1,5 @@
+# reads the names of the test balanced subsample and unzips test images into the '../input/test/' folder
+
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
